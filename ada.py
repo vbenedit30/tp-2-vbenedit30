19068891edd3517ba009bcf1eec5a0a58cf4d287
vbenedit30 <vbenedit@mail.austral.edu.ar>
2025-04-08 18:10:52 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.title()} {last_name.title()}")
+    print(f"{first_name.upper()} {last_name.upper()}")
+    print(f"\t{first_name.lower()} {last_name.lower()}")
+ada()
